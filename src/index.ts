da590c8161b70dc25f877a6ca5af4f35d2368d89
Ibrahim Khawar <ikb1703@gmail.com>
2022-02-10 20:25:42 -0500

    F

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(authenticateRefreshToken);
+// app.use(authenticateRefreshToken);
 
 const PORT = process.env.PORT || 5001;
 
