62a4714f82b7a8f53c03ef8dadd6b8a6c1a5b89f
Ibrahim Khawar <ikb1703@gmail.com>
2022-02-13 19:18:42 -0500

    F

--- src/helpers/validateData.ts
+++ src/helpers/validateData.ts
@@ -22,14 +22,18 @@ const validateData = async (
 		// Validate existence
 		const existenceErrors = validateDataExistence(row, rules, fields);
 		if (existenceErrors.errorCount) {
-			csvJSON[i].Error += `\"${existenceErrors.payload.errors[0].message}\"\n`;
+			csvJSON[i].Error += `\"${existenceErrors.payload.errors[0].message}\n${
+				!csvJSON[i].Error.length ? '"' : ""
+			}`;
 			isErrors = true;
 		}
 
 		// Validate Datatype
 		const dataTypeErrors = validateDataType(row, rules, fields);
 		if (dataTypeErrors.errorCount) {
-			csvJSON[i].Error += `\"${dataTypeErrors.payload.errors[0].message}\n`;
+			csvJSON[i].Error += `\"${dataTypeErrors.payload.errors[0].message}\n${
+				!csvJSON[i].Error.length ? '"' : ""
+			}`;
 			isErrors = true;
 		}
 	}
