8e6ad554de633d87b4e44b12093a6b435891383f
Ibrahim Khawar <ikb1703@gmail.com>
2022-03-18 23:07:52 -0400

    F

--- src/jobs/worker.ts
+++ src/jobs/worker.ts
@@ -4,10 +4,6 @@ import { JobData } from "../types";
 
 const queue = new Queue<JobData>("validation", process.env.REDIS_URL);
 
-console.log("WORKER RUNNING");
-
-console.log(queue, "FJLSLFDSK");
-
 queue.process(async (job) => {
 	console.log(
 		job.data,
