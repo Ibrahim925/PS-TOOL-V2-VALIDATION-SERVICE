7f1d8659e41f137de93cc6ed1d08c5f23bf912a3
Ibrahim Khawar <ikb1703@gmail.com>
2022-03-19 04:02:19 -0400

    F

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import "dotenv/config";
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "1TB" }));
 
 app.use(authenticateAccessToken);
 app.use("/validate", validateRouter);
