da5d79980b169f01e38cdb2a00701c7daffe48c8
Ibrahim Khawar <ikb1703@gmail.com>
2022-03-19 21:52:32 -0400

    F

--- src/controller/validate.controller.ts
+++ src/controller/validate.controller.ts
@@ -3,15 +3,16 @@ import { Response } from "express";
 import validateNewCSV from "../jobs/producer";
 import Queue from "bull";
 import AWS from "aws-sdk";
+import fs from "fs";
 import "dotenv/config";
 
-AWS.config.update({
-	region: "us-east-2",
-	credentials: {
-		accessKeyId: process.env.IAM_ACCESS_KEY,
-		secretAccessKey: process.env.IAM_SECRET_KEY,
-	},
-});
+// AWS.config.update({
+// 	region: "us-east-2",
+// 	credentials: {
+// 		accessKeyId: process.env.IAM_ACCESS_KEY,
+// 		secretAccessKey: process.env.IAM_SECRET_KEY,
+// 	},
+// });
 
 const queue = new Queue<JobData>("validation", process.env.REDIS_URL);
 
@@ -27,9 +28,9 @@ export const validate_data = async (
 ) => {
 	const { file, projectName, objectName } = req.body;
 
-	console.log(typeof file);
-
 	res.sendStatus(200);
+
+	fs.createReadStream(file);
 
 	// const s3 = new AWS.S3();
 	// const params = {
